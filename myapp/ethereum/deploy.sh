fdcef51ca46560b74ee0a1718cd27ac333eb59ef
zzilovey@gmail.com <root@ip-172-31-43-175.us-east-2.compute.internal>
2018-03-09 13:15:23 +0000

    add kudu and ethereum

---
+++ myapp/ethereum/deploy.sh
@@ -0,0 +1,4 @@
+helm install --name ethereum https://japoonhelmstrg.blob.core.windows.net/public/ethereum-0.1.0.tgz \
+   --set geth.account.publicKey=0a4ae8feaf6a425cdcad462b66fb5ae5cb930b64bf04380af9925d087053a3dce9bbb63f74a1d804c7ea4bf2c58476f3c6592fea0aa9a734e4717b84b8f06b25 \
+   --set geth.account.privateKey=5d556aebba81402a6d9d71adaba74e1e8f56f90308d304af847d9487b587a263 \
+   --set geth.account.secret=D@zzilovey9-
